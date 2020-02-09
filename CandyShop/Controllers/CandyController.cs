d69f3f3acfb9fdc930f199199bd0e0e7cd54d139
Lars Lundin <larslundin72@live.se>
2020-02-09 17:36:24 +0100

    Add project files.

---
+++ CandyShop/Controllers/CandyController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CandyShop.Models;
+
+namespace CandyShop.Controllers
+{
+    public class CandyController : Controller
+    {
+
+        private readonly ICandyRepository _candyRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public CandyController(ICandyRepository candyRepository, ICategoryRepository categoryRepository)
+        {
+            _candyRepository = candyRepository;
+            _categoryRepository = categoryRepository;
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public ViewResult List()
+        {
+            IEnumerable<Candy> candyList = _candyRepository.GetAllCandy;
+            return View(candyList);
+        }
+    }
+}
---
+++ CandyShop/Models/ICandyRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CandyShop.Models
+{
+    public interface ICandyRepository
+    {
+        IEnumerable<Candy> GetAllCandy { get;} 
+        IEnumerable<Candy> GetCandyOnSale { get; }
+        public Candy GetCandyById(int candyId);
+    }
+}
